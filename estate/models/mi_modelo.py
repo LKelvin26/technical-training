eb5d70d612f9d63724256d791066c27f47660df8
LKelvin26 <kevinm@pintacomex.mx>
2024-09-09 12:58:52 -0600

    fix infinito

--- estate/models/mi_modelo.py
+++ estate/models/mi_modelo.py
@@ -5,15 +5,5 @@ class MiModelo(models.Model):
     _description = 'Descripción de Mi Modelo'
 
     name = fields.Char(string='Nombre')
-    description =fields.Text()
-    postcode = fields.Char()
-    date_availability =fields.Float()
-    expected_price = fields.Date()
-    selling_price = fields.Float()
-    bedrooms = fields.integer()
-    living_area = fields.integer()
-    facades = fields.integer()
-    garage = fields.Boolean()
-    garden_area = fields.integer()
-    garden_orientation=fields.Selection()
+   
     
