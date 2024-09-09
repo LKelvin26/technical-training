5a294a8aafce85ace4466ff905289a0b3fec008e
LKelvin26 <kevinm@pintacomex.mx>
2024-09-09 12:20:09 -0600

    fix

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,7 +4,6 @@
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
-        'views/mi_modelo_view.xml'
     ],
     "installable": True,
     'license': 'LGPL-3',
--- estate/models.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class MiModelo(models.Model):
-    _name = 'mi.modelo'
-    _description = 'Descripción del modelo'
-
-    name = fields.Char(string='Nombre')
-    description = fields.Text(string='Descripción')
