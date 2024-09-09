2abc505a846984c555b1e0a8e198ab044925fc84
LKelvin26 <kevinm@pintacomex.mx>
2024-09-09 13:20:08 -0600

    models intento

--- estate/models/test_model.py
+++ estate/models/test_model.py
@@ -5,5 +5,20 @@ class TestModel(models.Model):
     _description = 'Descripción de Mi Modelo'
 
     name = fields.Char(string='Nombre')
-   
-    
+    description = fields.Text(string='Descripción')
+    postcode = fields.Char(string='Código Postal')
+    date_availability = fields.Date(string='Fecha de Disponibilidad')
+    expected_price = fields.Float(string='Precio Esperado')
+    selling_price = fields.Float(string='Precio de Venta')
+    bedrooms = fields.Integer(string='Número de Habitaciones')
+    living_area = fields.Integer(string='Área de Estar')
+    facades = fields.Integer(string='Número de Fachadas')
+    garage = fields.Boolean(string='Garage')
+    garden_area = fields.Integer(string='Área del Jardín')
+    garden_orientation = fields.Selection([
+        ('north', 'Norte'),
+        ('south', 'Sur'),
+        ('east', 'Este'),
+        ('west', 'Oeste')
+    ], string='Orientación del Jardín')
+
