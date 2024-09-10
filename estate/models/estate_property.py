9b54ed51a9bd4f3b43c97519a8d36929a0c76ac5
LKelvin26 <kevinm@pintacomex.mx>
2024-09-10 12:37:55 -0600

    xd

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from odoo import models, fields
 
+
 class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Estate Property'
@@ -10,7 +11,6 @@ class EstateProperty(models.Model):
     date_availability = fields.Date(string='Date of Availability', default=lambda self: fields.Date.today() + timedelta(days=90))
     selling_price = fields.Float(string='Selling Price')
     postcode = fields.Integer(string="Post Code")
-    # Campos reservados
     active = fields.Boolean(string='Active', default=True)
     state = fields.Selection([
         ('new', 'New'),
@@ -19,3 +19,8 @@ class EstateProperty(models.Model):
         ('sold', 'Sold'),
         ('canceled', 'Canceled')
     ], string='State', required=True, default='new', copy=False)
+    
+    # New fields
+    property_type_id = fields.Many2one('estate.property.type', string='Property Type')
+    buyer_id = fields.Many2one('res.partner', string='Buyer')
+    salesperson_id = fields.Many2one('res.users', string='Salesperson', default=lambda self: self.env.user)
