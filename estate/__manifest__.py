781de931923792d6401314310514fc5cc9c1655f
LKelvin26 <kevinm@pintacomex.mx>
2024-09-10 13:17:12 -0600

    xd

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,7 +6,7 @@
     "data": [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
-        'views/estate_property_tag.xml',
+        'views/estate_property_tag_views.xml',
         'views/estate_menus.xml',
        
         
