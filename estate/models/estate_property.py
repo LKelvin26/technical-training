b4c021efe62bbe6257532a2cdf8fa3fe78bc22ac
LKelvin26 <kevinm@pintacomex.mx>
2024-09-10 15:26:44 -0600

    xdddxdxd

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,12 +1,11 @@
 from datetime import datetime, timedelta
 from odoo import models, fields
 
-
 class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Estate Property'
 
-    name = fields.Char(string='Name')
+    name = fields.Char(string='Name', required=True)
     bedrooms = fields.Integer(string='Number of Bedrooms', default=2)
     date_availability = fields.Date(string='Date of Availability', default=lambda self: fields.Date.today() + timedelta(days=90))
     selling_price = fields.Float(string='Selling Price')
@@ -20,12 +19,11 @@ class EstateProperty(models.Model):
         ('canceled', 'Canceled')
     ], string='State', required=True, default='new', copy=False)
     
-    # New fields
+    # Relationship fields
     property_type_id = fields.Many2one('estate.property.type', string='Property Type')
     buyer_id = fields.Many2one('res.partner', string='Buyer')
     salesperson_id = fields.Many2one('res.users', string='Salesperson', default=lambda self: self.env.user)
 
+    # Tags and offers
     tag_ids = fields.Many2many('estate.property.tag', string='Tags')
-
-    # One2many field for offers
     offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Offers')
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -4,10 +4,11 @@ class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
     _description = 'Estate Property Offer'
 
-    price = fields.Float(string='Price')
+    price = fields.Float(string='Price', required=True)
     status = fields.Selection([
         ('accepted', 'Accepted'),
         ('refused', 'Refused')
-    ], string='Status', default='refused')
+    ], string='Status', default='refused', copy=False)
+    
     partner_id = fields.Many2one('res.partner', string='Partner', required=True)
     property_id = fields.Many2one('estate.property', string='Property', required=True)
