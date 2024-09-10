c24346a04dcae6f10ebd984ff98342fa48af82d0
LKelvin26 <kevinm@pintacomex.mx>
2024-09-10 10:04:27 -0600

    a

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,11 +1,21 @@
+from datetime import datetime, timedelta
 from odoo import models, fields
-from datetime import timedelta
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
-    _description = 'Real Estate Property'
+    _description = 'Estate Property'
 
-    name = fields.Char(string='Title', required=True)
-    selling_price = fields.Float(string='Selling Price', readonly=True, copy=False)
-    date_availability = fields.Date(string='Availability Date', copy=False, default=lambda self: fields.Date.today() + timedelta(days=90))
-    bedrooms = fields.Integer(string='Bedrooms', default=2)
+    name = fields.Char(string='Name')
+    bedrooms = fields.Integer(string='Number of Bedrooms', default=2)
+    date_availability = fields.Date(string='Date of Availability', default=lambda self: fields.Date.today() + timedelta(days=90))
+    selling_price = fields.Float(string='Selling Price')
+
+    # Campos reservados
+    active = fields.Boolean(string='Active', default=True)
+    state = fields.Selection([
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('canceled', 'Canceled')
+    ], string='State', required=True, default='new', copy=False)
