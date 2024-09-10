fdd90e33b2c774ceee497429b4688cf94c896f7a
LKelvin26 <kevinm@pintacomex.mx>
2024-09-10 09:22:36 -0600

    prueba estate

--- estate/models/__init__.py
+++ estate/models/__init__.py
@@ -1,2 +1,3 @@
 from . import test_model
 from . import mi_modelo
+from . import estate_property
---
+++ estate/models/estate_property.py
@@ -0,0 +1,28 @@
+from odoo import models, fields
+
+class EstateProperty(models.Model):
+    _name = 'estate.property'
+    _description = 'Descripción de Mi Modelo'
+
+    name = fields.Char(string='Nombre')
+    description = fields.Text(string='Descripción')
+    postcode = fields.Char(string='Código Postal')
+    date_availability = fields.Date(string='Fecha de Disponibilidad',default=fields.Datetime.now)
+    expected_price = fields.Float(string='Precio Esperado')
+    selling_price = fields.Float(string='Precio de Venta')
+    bedrooms = fields.Integer(string='Número de Habitaciones',default=2)
+    living_area = fields.Integer(string='Área de Estar')
+    facades = fields.Integer(string='Número de Fachadas')
+    garage = fields.Boolean(string='Garage')
+    garden_area = fields.Integer(string='Área del Jardín')
+    garden_orientation = fields.Selection([
+        ('north', 'Norte'),
+        ('south', 'Sur'),
+        ('east', 'Este'),
+        ('west', 'Oeste')
+    ], string='Orientación del Jardín')
+    status = fields.Selection([
+    ('new', 'Nueva'),
+    ('confirmed', 'Confirmada'),
+    ('canceled', 'Cancelada')
+], string='Estatus', required=True, default='new', copy=False)
